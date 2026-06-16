940cd74321a8e6398d94017103a16949d24e2a7f
Saul <jacktrotamundos95@gmail.com>
2026-06-15 22:11:07 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const audio = document.getElementById("audio");
+const img = document.getElementById("prenderaudio");
+const blanco = document.getElementById("blanco");
+
+img.addEventListener("click", function() {
+    audio.play();
+    blanco.classList.add("visible");
+});
+
+audio.onended = function() {
+    window.location.href="../mundo/mundo.html"
+};
